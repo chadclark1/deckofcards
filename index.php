8ed0fe12411847ebf72277c03c4414f379ac33ac
Chad Clark <chadclark1@gmail.com>
2016-05-30 19:48:13 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php session_start();
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Cards</title>
+</head>
+	<body>
+		<form action="process.php" method="post">
+			<input type="hidden" name="action" value="deal" >
+			<input type="submit" value="deal">
+		</form>
+		<form action="process.php" method="post">
+			<input type="hidden" name="action" value="reset" >
+			<input type="submit" value="reset">
+		</form>
+		<form action="process.php" method="post">
+			<input type="hidden" name="action" value="shuffle" >
+			<input type="submit" value="shuffle">
+		</form>
+		<?php
+			if(isset($_SESSION['pic'])){
+				?>
+				<img src="<?php echo $_SESSION['pic']; ?>">
+			<?php
+			}
+		?>
+	</body>
+</html>
